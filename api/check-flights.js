dffee2c73ba9e58d70c7d58caea4504b65cbe477
Ryan Wood <ryan@livedifferent.com>
2025-11-26 10:29:40 -0600

    Debug check-flights with trivial handler

--- api/check-flights.js
+++ api/check-flights.js
@@ -124,9 +124,14 @@ async function triggerBuild() {
  * Main handler function
  */
 module.exports = async (req, res) => {
+    return res.status(200).json({ status: 'ok', message: 'Check flights endpoint is reachable' });
+    /*
     try {
         // Fetch latest flight from WeGlide
         const latestFlight = await fetchLatestFlight();
+    ...
+    */
+};
 
         if (!latestFlight) {
             return res.status(200).json({
